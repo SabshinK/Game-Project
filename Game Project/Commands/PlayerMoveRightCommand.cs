1984a75c43b12d27ce8911bb8d1a5ad63b0f90bf
aj-aadyajain <86252327+aj-aadyajain@users.noreply.github.com>
2022-02-11 12:05:14 -0500

    PlayerMoveLeft & Right Commands added
    The commands for moving left and right. As per the previous version, there is a bit of refactoring that might be needed in terms of readability and maintainability, but I'm not sure how to do that. Awaiting code review of 2/11/2022 for that. Will be adding the PlayerMoveLeft.cs and PlayerMoveRight.cs soon.

---
+++ Game Project/Commands/PlayerMoveRightCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_Project
+{
+    class PlayerMoveRightCommand : ICommand
+    {
+        private Game1 myGame;
+
+        public PlayerMoveRightCommand(Game1 game)
+        {
+            myGame = game;
+        }
+
+        public void Execute()
+        {
+            // Three dots is nasty here, this all should be refactored
+            myGame.setSprite(new PlayerMoveRight(myGame.PlayerTexture, myGame.Location, myGame.GraphicsDevice.Viewport.Bounds,
+                myGame.rows, myGame.columns));
+        }
+    }
+}
