0ad6b9c517f9d97b94ab1635cbdb8e0fa581c648
aj-aadyajain <86252327+aj-aadyajain@users.noreply.github.com>
2022-02-11 11:42:30 -0500

    Draft version of IPlayer interface
    Only has the update function which both the PlayerMoveLeft and PlayerMoveRight.cs will need to implement. Will add more functions based on the code reviews and todos.

---
+++ Game Project/Interfaces/IPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_Project.Interfaces
+{
+    internal interface IPlayer
+    {
+
+        public void Update(); //both move left and moveRight would need to implement this. 
+    }
+}
