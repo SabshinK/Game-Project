feb513e7ab8f81c91946d0cf7ee1690461a42912
wattersrapids <98502771+wattersrapids@users.noreply.github.com>
2022-02-15 22:22:40 -0500

    Create IObjectInteractable.cs
    ObjectInteractable interface, where the Interact() function is declared.

---
+++ Game Project/Interfaces/IObjectInteractable.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_Project.Interfaces
+{
+    internal Interface IObjectInteractable
+    {
+        public void Interact(); // Update any appropriate states for when the player collides with another object.
+    }
+}
