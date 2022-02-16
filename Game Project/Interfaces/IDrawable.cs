9b629b9d5ed4d70097f265c8b3b598c6e8731646
wattersrapids <98502771+wattersrapids@users.noreply.github.com>
2022-02-15 22:23:33 -0500

    Create IDrawable.cs
    IDrawable interface, where the method Draw() is declared.

---
+++ Game Project/Interfaces/IDrawable.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_Project.Interfaces
+{
+    internal Interface IDrawable
+    {
+        public void Draw(); // Update any appropriate states for when the player collides with another object.
+    }
+}
