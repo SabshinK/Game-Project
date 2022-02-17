6f06bc8f7c220c73fb2471578150709b5434dbb1
aj-aadyajain <86252327+aj-aadyajain@users.noreply.github.com>
2022-02-17 14:06:39 -0500

    Update PlayerMoveLeftCommand.cs
    This should work with the Sprite factory parameters. Feel free to critique it!

--- Game Project/Commands/PlayerMoveLeftCommand.cs
+++ Game Project/Commands/PlayerMoveLeftCommand.cs
@@ -1,23 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Game_Project
 {
-    class PlayerMoveLeftCommand : ICommand
+    class PlayerMoveRightCommand : ICommand
     {
         private Game1 myGame;
 
-        public PlayerMoveLeftCommand(Game1 game)
+        public PlayerMoveRightCommand(Game1 game)
         {
             myGame = game;
         }
 
         public void Execute()
         {
+
             // Three dots is nasty here, this all should be refactored
-            myGame.setSprite(new PlayerMoveLeft(myGame.PlayerTexture, myGame.Location, myGame.GraphicsDevice.Viewport.Bounds,
-                myGame.rows, myGame.columns));
+            myGame.setSprite(new PlayerMoveLeft(myGame.Location));
         }
     }
 }
