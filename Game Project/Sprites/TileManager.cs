05194c771dddd05a3da9501d9ac1680ba71daef1
wattersrapids <98502771+wattersrapids@users.noreply.github.com>
2022-02-17 15:44:28 -0500

    Update TileManager.cs
    changed indentation

--- Game Project/Sprites/TileManager.cs
+++ Game Project/Sprites/TileManager.cs
@@ -8,36 +8,36 @@ namespace Game_Project.Sprites
 {
     class TileManager
     {
-      private int tileNumber;
-      private string[] tileArray;
-      private int lengthOfArray = 3;
+        private int tileNumber;
+        private string[] tileArray;
+        private int lengthOfArray = 3;
       
-      public TileManager
-      {
-        tileNumber = 0;
-        tileArray = new string[lengthOfArray] {"TileZero", "TileOne", "TileTwo"};
-        SpriteFactory.instance.CreateSprite(tileArray[tileNumber]);
-      }
-      
-      public void NextTile
-      {
-        if (tileNumber == (lengthOfArray - 1)) {
-          tileNumber = 0;
-        } else {
-          tileNumber++;
-        }        
-        SpriteFactory.instance.CreateSprite(tileArray[tileNumber]);
-      }
-      
-      public void PreviousTile
-      {
-        if (tileNumber == 0) {
-          tileNumber = (lengthOfArray - 1);
-        } else {
-          tileNumber--;
+        public TileManager
+        {
+            tileNumber = 0;
+            tileArray = new string[lengthOfArray] {"TileZero", "TileOne", "TileTwo"};
+            SpriteFactory.instance.CreateSprite(tileArray[tileNumber]);
         }
-        SpriteFactory.instance.CreateSprite(tileArray[tileNumber]);
-      }
+      
+        public void NextTile
+        {
+            if (tileNumber == (lengthOfArray - 1)) {
+                tileNumber = 0;
+            } else {
+                tileNumber++;
+            }        
+            SpriteFactory.instance.CreateSprite(tileArray[tileNumber]);
+        }
+      
+        public void PreviousTile
+        {
+            if (tileNumber == 0) {
+                tileNumber = (lengthOfArray - 1);
+            } else {
+                tileNumber--;
+            }
+            SpriteFactory.instance.CreateSprite(tileArray[tileNumber]);
+        }
       
     }
     
