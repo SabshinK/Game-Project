a5fbf82b567aeda96a614082ca8c5d5cccb63ae8
wattersrapids <98502771+wattersrapids@users.noreply.github.com>
2022-02-17 15:41:55 -0500

    Update PlayerManager.cs
    changed indentation

--- Game Project/Sprites/PlayerManager.cs
+++ Game Project/Sprites/PlayerManager.cs
@@ -8,47 +8,47 @@ namespace Game_Project.Sprites
 {
     class PlayerManager
     {
-      public IPlayer state;
+        public IPlayer state;
       
-      private int Health;
-      private string animationToCreate;
-      public boolean faceRight;
+        private int Health;
+        private string animationToCreate;
+        public boolean faceRight;
       
-      // Constructor
-      public PlayerManager()
-      {
-        state = new RightIdle(this);
-        animationToCreate = "RightIdle";
-        faceRight = true;
-        SpriteFactory.instance.CreateSprite(animationToCreate);
+        // Constructor
+        public PlayerManager()
+        {
+            state = new RightIdle(this);
+            animationToCreate = "RightIdle";
+            faceRight = true;
+            SpriteFactory.instance.CreateSprite(animationToCreate);
         
-        Health = 3;
-      }
+            Health = 3;
+        }
         
-      // CompleteRight and CompleteLeft will be called after a move, attack, or damage animation, depending on which direction the sprite was facing.
-      public void CompleteRight()
-      {
-        state.RightIdle();
-        animationToCreate = "RightIdle";
-        faceRight = true;
-        SpriteFactory.instance.CreateSprite(animationToCreate);
-      }
+        // CompleteRight and CompleteLeft will be called after a move, attack, or damage animation, depending on which direction the sprite was facing.
+        public void CompleteRight()
+        {
+            state.RightIdle();
+            animationToCreate = "RightIdle";
+            faceRight = true;
+            SpriteFactory.instance.CreateSprite(animationToCreate);
+        }
         
-      public void CompleteLeft()
-      {
-        state.LeftIdle();
-        animationToCreate = "LeftIdle";
-        faceRight = false;
-        SpriteFactory.instance.CreateSprite(animationToCreate);
-      }
+        public void CompleteLeft()
+        {
+            state.LeftIdle();
+            animationToCreate = "LeftIdle";
+            faceRight = false;
+            SpriteFactory.instance.CreateSprite(animationToCreate);
+        }
         
-      // For Sprint 2, taking damage will be shown when we press 'e', but for future sprints, this will be triggered by contact with an enemy.
-      public void DamageTaken()
-      {
-        Health--;
-        state.TakeDamage();
-        animationToCreate = "TakeDamage";
-        SpriteFactory.instance.CreateSprite(animationToCreate);
-      } 
+        // For Sprint 2, taking damage will be shown when we press 'e', but for future sprints, this will be triggered by contact with an enemy.
+        public void DamageTaken()
+        {
+            Health--;
+            state.TakeDamage();
+            animationToCreate = "TakeDamage";
+            SpriteFactory.instance.CreateSprite(animationToCreate);
+        } 
     }
 }
