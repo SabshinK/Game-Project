4941461b83a1b8c380a0b4fa9958ba11dec3e28e
aj-aadyajain <86252327+aj-aadyajain@users.noreply.github.com>
2022-02-17 14:07:22 -0500

    Updated PlayerMoveRightCommand.cs
    This should work with Sprite Factory

--- Game Project/Commands/PlayerMoveRightCommand.cs
+++ Game Project/Commands/PlayerMoveRightCommand.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -15,9 +15,9 @@ namespace Game_Project
 
         public void Execute()
         {
+
             // Three dots is nasty here, this all should be refactored
-            myGame.setSprite(new PlayerMoveRight(myGame.PlayerTexture, myGame.Location, myGame.GraphicsDevice.Viewport.Bounds,
-                myGame.rows, myGame.columns));
+            myGame.setSprite(new PlayerMoveRight(myGame.Location));
         }
     }
 }
