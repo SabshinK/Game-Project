adb72768f4d8d05ef34283a32422b71ed30d7e6a
aj-aadyajain <86252327+aj-aadyajain@users.noreply.github.com>
2022-02-19 08:22:11 -0500

    Uploaded ItemManager.cs
    this file should be able to toggle between different items. most code is from tilemanager.cs

---
+++ Game Project/ItemManager.cs
@@ -0,0 +1,74 @@
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_Project
+{
+    public class ItemManager
+    {
+        private ISprite sprite;
+
+        private int itemNumber;
+        private List<ISprite> itemList;
+        private const int lengthOfList = 3;
+        private Vector2 location;
+
+        public ItemManager()
+        {
+            itemNumber = 0;
+            itemList = new List<ISprite>();
+            LoaditemList();
+            sprite = itemList[itemNumber];
+        }
+
+        public void NextItem()
+        {
+            if (itemNumber == (lengthOfList - 1))
+            {
+                itemNumber = 0;
+            }
+            else
+            {
+                itemNumber++;
+            }
+            sprite = itemList[itemNumber];
+        }
+
+        public void Previousitem()
+        {
+            if (itemNumber == 0)
+            {
+                itemNumber = (lengthOfList - 1);
+            }
+            else
+            {
+                itemNumber--;
+            }
+            sprite = itemList[itemNumber];
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch, location);
+        }
+
+        public void LoaditemList()
+        {
+            itemList.Add(SpriteFactory.Instance.CreateSprite("fairyGeneric"));
+            itemList.Add(SpriteFactory.Instance.CreateSprite("stopwatch"));
+            itemList.Add(SpriteFactory.Instance.CreateSprite("rupee"));
+            itemList.Add(SpriteFactory.Instance.CreateSprite("redPotion"));
+            itemList.Add(SpriteFactory.Instance.CreateSprite("bluePotion"));
+            itemList.Add(SpriteFactory.Instance.CreateSprite("dungeonMap"));
+            itemList.Add(SpriteFactory.Instance.CreateSprite("bait"));
+            itemList.Add(SpriteFactory.Instance.CreateSprite("bowAndArrow"));
+            itemList.Add(SpriteFactory.Instance.CreateSprite("smallKey"));
+            itemList.Add(SpriteFactory.Instance.CreateSprite("compass"));
+            itemList.Add(SpriteFactory.Instance.CreateSprite("triforce"));
+        }
+
+    }
+
+}
