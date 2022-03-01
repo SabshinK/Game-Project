7ed53c52008f6578fda7828cc951401b085a8df9
SabshinK <sebastiantking@gmail.com>
2022-03-01 12:29:19 -0500

    Modified SpriteFactory and GenericSprite to take both a scale and an animation speed, GenericSprite now takes a tuple as it's parameter. Added the beginnings of classes for loading levels and parsing XML documents to get content and level data

---
+++ Game Project/Data/XmlParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+namespace Game_Project
+{
+    class XmlParser
+    {
+        private XmlReader reader;
+
+        public XmlParser()
+        {
+            // load reader settings with XML Schema validation
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Schemas.Add("urn:frames-schema", "frames.xsd");
+            settings.ValidationType = ValidationType.Schema;
+
+            reader = XmlReader.Create("frames.xml", settings);
+        }
+
+        public void Parse()
+        {
+
+        }
+    }
+}
