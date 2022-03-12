44b201e54f67068a6d5eea400ad06fb6daa39eb5
SabshinK <sebastiantking@gmail.com>
2022-03-11 20:18:02 -0500

    objects that create projectiles now use the UPO

--- Game Project/Enemies/GelEnemy.cs
+++ Game Project/Enemies/GelEnemy.cs
@@ -4,7 +4,6 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using static Game_Project.IEnemyStateMachine;
-using Game_Project.Physics;
 
 namespace Game_Project
 {
