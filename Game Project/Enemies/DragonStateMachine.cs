a042205c7242f6550dd300289b955dee61ca7840
AJ Waizmann <waizmann.4@buckeyemail.osu.edu>
2022-03-12 09:27:35 -0500

    Dragon refactors

--- Game Project/Enemies/DragonStateMachine.cs
+++ Game Project/Enemies/DragonStateMachine.cs
@@ -19,6 +19,8 @@ namespace Game_Project
 
         public void ChangeDirection()
         {
+
+            dragonAction = actions.moving;
             if (dragonDirection.Equals(direction.right)){
                 dragonDirection = direction.left;
             }
