4090d16d8fbd8a2f6a0f7a90d2118a26c0b201cc
AJ Waizmann <waizmann.4@buckeyemail.osu.edu>
2022-04-05 13:21:52 -0400

    Added generic enemy commands

---
+++ Game Project/Commands/EnemyChangeDirectionCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_Project
+{
+    class EnemyChangeDirectionCommand : ICommand
+    {
+        private IEnemy enemy;
+
+        public EnemyChangeDirectionCommand(IEnemy Enemy)
+        {
+            enemy = Enemy;
+        }
+        public void Execute()
+        {
+            enemy.ChangeDirection();
+        }
+    }
+}
