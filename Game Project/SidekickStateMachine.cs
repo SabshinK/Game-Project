ad55008c44099431d010d7a98f61c555d86f464b
aj-aadyajain <86252327+aj-aadyajain@users.noreply.github.com>
2022-04-15 23:27:58 -0400

    AI Sidekick Files Draft
    Has the movement stuff done for AISidekick. Doesn't have the attack functionality yet. Will work with Rachel for that. Hopefully works!

---
+++ Game Project/SidekickStateMachine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Game_Project.ISidekickStateMachine;
+
+namespace Game_Project.Sidekick
+{
+    class SidekickStateMachine :ISidekickStateMachine
+    {
+
+        private Tuple<bool, bool> sidekickState;
+        private bool FacingRight;
+        public bool Following;
+        private Player player;
+
+        public SidekickStateMachine()
+        {
+        }
+
+        public void Attack()
+        {
+            //Attack is random
+
+        }
+
+        public void Follow()
+        {
+            Following = true;
+            FacingRight = player.FaceRight;
+
+        }
+
+        public Tuple<bool, bool> getState()
+        {
+            sidekickState = new Tuple<bool, bool>(Following, FacingRight);
+            return sidekickState;
+        }
+
+        public void Stay()
+        {
+            Following = false;
+            FacingRight = player.FaceRight;
+        }
+    }
+}
